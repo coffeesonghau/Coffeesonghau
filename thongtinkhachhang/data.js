444f8e9d511db713775c5a33ac8d3097903331cb
coffeesonghau <coffeesonghaubl@gmail.com>
2026-03-03 15:09:39 +0700

    Update: 2026-03-03 15:09

--- thongtinkhachhang/data.js
+++ thongtinkhachhang/data.js
@@ -11,25 +11,25 @@ const productData = {
     // Các mức giá (VNĐ) được chia theo 3 nhóm như bạn yêu cầu
     products: [
         // Dòng 500g
-        { id: "tt1_500", name: "Truyền Thống 1", weight: "500g", prices: { ban_le: 100000, dai_ly_quan: 80000, he_thong_npp: 75000 } },
-        { id: "tt2_500", name: "Truyền Thống 2", weight: "500g", prices: { ban_le: 110000, dai_ly_quan: 90000, he_thong_npp: 85000 } },
-        { id: "mm1_500", name: "Mạnh Mẽ 1", weight: "500g", prices: { ban_le: 120000, dai_ly_quan: 95000, he_thong_npp: 90000 } },
-        { id: "mm2_500", name: "Mạnh Mẽ 2", weight: "500g", prices: { ban_le: 130000, dai_ly_quan: 100000, he_thong_npp: 95000 } },
-        { id: "cd_500", name: "Cận Đại", weight: "500g", prices: { ban_le: 150000, dai_ly_quan: 120000, he_thong_npp: 115000 } },
+        { id: "tt1_500", name: "Truyền Thống 1", weight: "500g", prices: { ban_le: 150000, dai_ly_quan: 130000, he_thong_npp: 120000 } },
+        { id: "tt2_500", name: "Truyền Thống 2", weight: "500g", prices: { ban_le: 120000, dai_ly_quan: 110000, he_thong_npp: 100000 } },
+        { id: "mm1_500", name: "Mạnh Mẽ 1", weight: "500g", prices: { ban_le: 110000, dai_ly_quan: 100000, he_thong_npp: 90000 } },
+        { id: "mm2_500", name: "Mạnh Mẽ 2", weight: "500g", prices: { ban_le: 90000, dai_ly_quan: 80000, he_thong_npp: 70000 } },
+        { id: "cd_500", name: "Cận Đại", weight: "500g", prices: { ban_le: 70000, dai_ly_quan: 60000, he_thong_npp: 50000 } },
 
         // Dòng 250g
-        { id: "pre_250", name: "Premium 2026", weight: "250g", prices: { ban_le: 200000, dai_ly_quan: 160000, he_thong_npp: 150000 } },
-        { id: "tt1_250", name: "Truyền Thống 1", weight: "250g", prices: { ban_le: 55000, dai_ly_quan: 45000, he_thong_npp: 40000 } },
-        { id: "tt2_250", name: "Truyền Thống 2", weight: "250g", prices: { ban_le: 60000, dai_ly_quan: 50000, he_thong_npp: 45000 } },
-        { id: "mm1_250", name: "Mạnh Mẽ 1", weight: "250g", prices: { ban_le: 65000, dai_ly_quan: 55000, he_thong_npp: 50000 } },
-        { id: "mm2_250", name: "Mạnh Mẽ 2", weight: "250g", prices: { ban_le: 70000, dai_ly_quan: 60000, he_thong_npp: 55000 } },
-        { id: "cd_250", name: "Cận Đại", weight: "250g", prices: { ban_le: 80000, dai_ly_quan: 65000, he_thong_npp: 60000 } },
+        { id: "pre_250", name: "Premium 2026", weight: "250g", prices: { ban_le: 100000, dai_ly_quan: 95000, he_thong_npp: 90000 } },
+        { id: "tt1_250", name: "Truyền Thống 1", weight: "250g", prices: { ban_le: 75000, dai_ly_quan: 65000, he_thong_npp: 60000 } },
+        { id: "tt2_250", name: "Truyền Thống 2", weight: "250g", prices: { ban_le: 60000, dai_ly_quan: 55000, he_thong_npp: 50000 } },
+        { id: "mm1_250", name: "Mạnh Mẽ 1", weight: "250g", prices: { ban_le: 55000, dai_ly_quan: 50000, he_thong_npp: 45000 } },
+        { id: "mm2_250", name: "Mạnh Mẽ 2", weight: "250g", prices: { ban_le: 70000, dai_ly_quan: 40000, he_thong_npp: 35000 } },
+        { id: "cd_250", name: "Cận Đại", weight: "250g", prices: { ban_le: 35000, dai_ly_quan: 30000, he_thong_npp: 25000 } },
 
         // Dòng 100g
-        { id: "tt1_100", name: "Truyền Thống 1", weight: "100g", prices: { ban_le: 25000, dai_ly_quan: 20000, he_thong_npp: 18000 } },
-        { id: "tt2_100", name: "Truyền Thống 2", weight: "100g", prices: { ban_le: 28000, dai_ly_quan: 22000, he_thong_npp: 20000 } },
-        { id: "mm1_100", name: "Mạnh Mẽ 1", weight: "100g", prices: { ban_le: 30000, dai_ly_quan: 25000, he_thong_npp: 22000 } },
-        { id: "mm2_100", name: "Mạnh Mẽ 2", weight: "100g", prices: { ban_le: 32000, dai_ly_quan: 27000, he_thong_npp: 24000 } },
-        { id: "cd_100", name: "Cận Đại", weight: "100g", prices: { ban_le: 35000, dai_ly_quan: 30000, he_thong_npp: 28000 } }
+        { id: "tt1_100", name: "Truyền Thống 1", weight: "100g", prices: { ban_le: 40000, dai_ly_quan: 40000, he_thong_npp: 40000 } },
+        { id: "tt2_100", name: "Truyền Thống 2", weight: "100g", prices: { ban_le: 34000, dai_ly_quan: 34000, he_thong_npp: 34000 } },
+        { id: "mm1_100", name: "Mạnh Mẽ 1", weight: "100g", prices: { ban_le: 32000, dai_ly_quan: 32000, he_thong_npp: 32000 } },
+        { id: "mm2_100", name: "Mạnh Mẽ 2", weight: "100g", prices: { ban_le: 28000, dai_ly_quan: 28000, he_thong_npp: 28000 } },
+        { id: "cd_100", name: "Cận Đại", weight: "100g", prices: { ban_le: 26000, dai_ly_quan: 26000, he_thong_npp: 26000 } }
     ]
 };
