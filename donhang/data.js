5b7048fa3ddcd25569b76f6ec3b2469c3a153f2f
coffeesonghau <coffeesonghaubl@gmail.com>
2026-03-29 09:02:35 +0700

    Update: 2026-03-29 09:02

--- donhang/data.js
+++ donhang/data.js
@@ -9,7 +9,6 @@ const productData = {
     },
     
     // Danh sách sản phẩm
-    // Các mức giá (VNĐ) được chia theo 3 nhóm như bạn yêu cầu
     products: [
         // Dòng 500g
         { id: "tt1_500", name: "Truyền Thống 1", weight: "500g", prices: { ban_le: 150000, dai_ly_quan: 130000, he_thong_npp: 120000 } },
@@ -22,9 +21,9 @@ const productData = {
         { id: "pre_250", name: "Premium 2026", weight: "250g", prices: { ban_le: 100000, dai_ly_quan: 100000, he_thong_npp: 100000 } },
         { id: "tt1_250", name: "Truyền Thống 1", weight: "250g", prices: { ban_le: 75000, dai_ly_quan: 65000, he_thong_npp: 60000 } },
         { id: "tt2_250", name: "Truyền Thống 2", weight: "250g", prices: { ban_le: 60000, dai_ly_quan: 55000, he_thong_npp: 50000 } },
-        { id: "mm1_250", name: "Mạnh Mẽ 1", weight: "250g", prices: { ban_le: 55000, dai_ly_quan: 50000, he_thong_npp: 45000 } },
-        { id: "mm2_250", name: "Mạnh Mẽ 2", weight: "250g", prices: { ban_le: 45000, dai_ly_quan: 40000, he_thong_npp: 35000 } },
-        { id: "cd_250", name: "Cận Đại", weight: "250g", prices: { ban_le: 40000, dai_ly_quan: 35000, he_thong_npp: 30000 } },
+        { id: "mm1_250", name: "Mạnh Mẽ 1", weight: "250g", prices: { ban_le: 50000, dai_ly_quan: 45000, he_thong_npp: 40000 } },
+        { id: "mm2_250", name: "Mạnh Mẽ 2", weight: "250g", prices: { ban_le: 40000, dai_ly_quan: 35000, he_thong_npp: 30000 } },
+        { id: "cd_250", name: "Cận Đại", weight: "250g", prices: { ban_le: 35000, dai_ly_quan: 30000, he_thong_npp: 25000 } },
 
         // Dòng 100g
         { id: "tt1_100", name: "Truyền Thống 1", weight: "100g", prices: { ban_le: 40000, dai_ly_quan: 40000, he_thong_npp: 40000 } },
