42f360c84b49c6c18e12a182d260b0d943856ffe
coffeesonghau <coffeesonghaubl@gmail.com>
2026-07-02 15:36:43 +0700

    add

--- push.bat
+++ push.bat
@@ -1,68 +1,39 @@
 @echo off
-setlocal enabledelayedexpansion
-title CoffeeSongHau Auto Git Push Pro
-
-cd /d "%~dp0"
-
-set BRANCH=master
-set LOGFILE=git_push_log.txt
-
+title CoffeeSongHau Git Auto Push
 color 0A
 
 echo ======================================
 echo       CoffeeSongHau Git Auto Push
 echo ======================================
-
-:: Tao commit message theo thoi gian
-for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyy-MM-dd_HH-mm-ss"') do set msg=Auto Update %%i
-
 echo.
-echo [1/5] Checking changes...
+
+:: Chuyen den thu muc project (doi lai duong dan neu can)
+cd /d "C:\Users%USERNAME%\Documents\CoffeeSongHau"
+
+:: Kiem tra co thay doi khong
+echo [1/4] Checking changes...
 git status
 
 echo.
-echo [2/5] Adding files...
+set /p msg=Nhap commit message:
+
+:: Add tat ca file
+echo.
+echo [2/4] Adding files...
 git add .
 
-:: Kiem tra co thay doi khong
-git diff --cached --quiet
-if %errorlevel% equ 0 (
-    echo No changes detected.
-    pause
-    exit /b
-)
-
+:: Commit
 echo.
-echo [3/5] Pulling latest from GitHub...
-git pull origin %BRANCH% --rebase
-if %errorlevel% neq 0 (
-    echo [ERROR] Pull failed. Resolve conflict manually.
-    pause
-    exit /b
-)
-
-echo.
-echo [4/5] Commiting...
+echo [3/4] Committing...
 git commit -m "%msg%"
-if %errorlevel% neq 0 (
-    echo [ERROR] Commit failed.
-    pause
-    exit /b
-)
 
+:: Push len branch master
 echo.
-echo [5/5] Pushing to GitHub...
-git push origin %BRANCH%
-if %errorlevel% neq 0 (
-    echo [ERROR] Push failed.
-    pause
-    exit /b
-)
-
-echo [%date% %time%] SUCCESS: %msg% >> %LOGFILE%
+echo [4/4] Pushing to GitHub...
+git push origin master
 
 echo.
 echo ======================================
-echo Push completed successfully!
+echo Push completed!
 echo ======================================
 pause
